393ec3da7be1526d93f5adfd10ba3bc5873dd413
jamielesouef <jlesouef@icloud.com>
2026-09-23 09:30:55 +1000

    start of desktop app

---
+++ desktop/homerun-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  homerun-app
+//
+//  Created by Jamie Le Souef on 23/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ desktop/homerun-app/homerun_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  homerun_appApp.swift
+//  homerun-app
+//
+//  Created by Jamie Le Souef on 23/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct homerun_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
